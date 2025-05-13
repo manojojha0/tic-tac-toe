3c6cd3b6074032e8dd681660d3a5cbad17927ee3
Manojojha01 <manojojha726@gmail.com>
2025-05-13 23:22:29 +0530

    play multi players tic-tac-toe

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,225 @@
+import React, { useState, useEffect } from "react";
+import Square from "./Square/Square";
+import { io } from "socket.io-client";
+import Swal from "sweetalert2";
+
+const renderFrom = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+const App = () => {
+  const [gameState, setGameState] = useState(renderFrom);
+  const [currentPlayer, setCurrentPlayer] = useState("circle");
+  const [finishedState, setFinishetState] = useState(false);
+  const [finishedArrayState, setFinishedArrayState] = useState([]);
+  const [playOnline, setPlayOnline] = useState(false);
+  const [socket, setSocket] = useState(null);
+  const [playerName, setPlayerName] = useState("");
+  const [opponentName, setOpponentName] = useState(null);
+  const [playingAs, setPlayingAs] = useState(null);
+
+  const checkWinner = () => {
+    for (let row = 0; row < gameState.length; row++) {
+      if (
+        gameState[row][0] === gameState[row][1] &&
+        gameState[row][1] === gameState[row][2]
+      ) {
+        setFinishedArrayState([row * 3 + 0, row * 3 + 1, row * 3 + 2]);
+        return gameState[row][0];
+      }
+    }
+
+    for (let col = 0; col < gameState.length; col++) {
+      if (
+        gameState[0][col] === gameState[1][col] &&
+        gameState[1][col] === gameState[2][col]
+      ) {
+        setFinishedArrayState([0 * 3 + col, 1 * 3 + col, 2 * 3 + col]);
+        return gameState[0][col];
+      }
+    }
+
+    if (
+      gameState[0][0] === gameState[1][1] &&
+      gameState[1][1] === gameState[2][2]
+    ) {
+      return gameState[0][0];
+    }
+
+    if (
+      gameState[0][2] === gameState[1][1] &&
+      gameState[1][1] === gameState[2][0]
+    ) {
+      return gameState[0][2];
+    }
+
+    const isDrawMatch = gameState.flat().every((e) => {
+      if (e === "circle" || e === "cross") return true;
+    });
+
+    if (isDrawMatch) return "draw";
+
+    return null;
+  };
+
+  useEffect(() => {
+    const winner = checkWinner();
+    if (winner) {
+      setFinishetState(winner);
+    }
+  }, [gameState]);
+
+  const takePlayerName = async () => {
+    const result = await Swal.fire({
+      title: "Enter your name",
+      input: "text",
+      showCancelButton: true,
+      inputValidator: (value) => {
+        if (!value) {
+          return "You need to write something!";
+        }
+      },
+    });
+
+    return result;
+  };
+
+  socket?.on("opponentLeftMatch", () => {
+    setFinishetState("opponentLeftMatch");
+  });
+
+  socket?.on("playerMoveFromServer", (data) => {
+    const id = data.state.id;
+    setGameState((prevState) => {
+      let newState = [...prevState];
+      const rowIndex = Math.floor(id / 3);
+      const colIndex = id % 3;
+      newState[rowIndex][colIndex] = data.state.sign;
+      return newState;
+    });
+    setCurrentPlayer(data.state.sign === "circle" ? "cross" : "circle");
+  });
+
+  socket?.on("connect", function () {
+    setPlayOnline(true);
+  });
+
+  socket?.on("OpponentNotFound", function () {
+    setOpponentName(false);
+  });
+
+  socket?.on("OpponentFound", function (data) {
+    setPlayingAs(data.playingAs);
+    setOpponentName(data.opponentName);
+  });
+
+  async function playOnlineClick() {
+    const result = await takePlayerName();
+    if (!result.isConfirmed) return;
+
+    const username = result.value;
+    setPlayerName(username);
+
+    const newSocket = io("http://localhost:3000", {
+      autoConnect: true,
+    });
+
+    newSocket.emit("request_to_play", {
+      playerName: username,
+    });
+
+    setSocket(newSocket);
+  }
+
+  if (!playOnline) {
+    return (
+      <div className="w-full h-screen flex justify-center items-center bg-gray-900 text-white">
+        <button
+          onClick={playOnlineClick}
+          className="bg-gray-600 hover:bg-green-400 text-white font-bold py-2 px-8 rounded-lg hover:text-black transition transform hover:scale-110"
+        >
+          Play Online
+        </button>
+      </div>
+    );
+  }
+
+  if (playOnline && !opponentName) {
+    return (
+      <div className="w-full h-screen flex justify-center items-center bg-gray-900 text-white">
+        <p className="text-2xl">Waiting for opponent...</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="min-h-screen flex flex-col items-center bg-gray-900 text-white py-10">
+      <div className="flex justify-between w-[440px] mb-6 text-center font-semibold">
+        <div
+          className={`p-1 w-[120px] rounded-bl-[50px] rounded-tr-[50px] bg-gray-500 ${
+            currentPlayer === playingAs ? "bg-[#3fa7f0]" : ""
+          }`}
+        >
+          {playerName}
+        </div>
+        <div
+          className={`p-1 w-[120px] rounded-bl-[50px] rounded-tr-[50px] bg-gray-500 ${
+            currentPlayer !== playingAs ? "bg-pink-400" : ""
+          }`}
+        >
+          {opponentName}
+        </div>
+      </div>
+
+      <h1 className="text-xl font-bold p-2 w-[340px] rounded-lg mb-4 bg-gray-700 text-center">
+        Tic Tac Toe
+      </h1>
+
+      <div className="grid grid-cols-3 gap-2">
+        {gameState.map((arr, rowIndex) =>
+          arr.map((e, colIndex) => (
+            <Square
+              socket={socket}
+              playingAs={playingAs}
+              gameState={gameState}
+              finishedArrayState={finishedArrayState}
+              finishedState={finishedState}
+              currentPlayer={currentPlayer}
+              setCurrentPlayer={setCurrentPlayer}
+              setGameState={setGameState}
+              id={rowIndex * 3 + colIndex}
+              key={rowIndex * 3 + colIndex}
+              currentElement={e}
+            />
+          ))
+        )}
+      </div>
+
+      {finishedState && finishedState !== "draw" && finishedState !== "opponentLeftMatch" && (
+        <h3 className="mt-4 text-2xl font-bold">
+          {finishedState === playingAs ? "You" : finishedState}🎉 won the game
+        </h3>
+      )}
+
+      {finishedState === "draw" && (
+        <h3 className="mt-4 text-2xl font-bold">Match Draw ☺️ try again </h3>
+      )}
+
+      {finishedState === "opponentLeftMatch" && (
+        <h3 className="mt-4 text-2xl font-bold">
+         🥇 You won the match.  🤡 Opponent has left.
+        </h3>
+      )}
+
+      {!finishedState && opponentName && (
+        <h3 className="mt-4 text-2xl font-bold">
+          You are playing against 🤼‍♂️ {opponentName}
+        </h3>
+      )}
+    </div>
+  );
+};
+
+export default App;
